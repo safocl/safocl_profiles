488855c3516b38e89e05a655f69f3e24fa0f9279
safocl <safocl88@gmail.com>
2017-12-24 20:53:49 +0400

    upd .config/luastatus/cryptocurr.lua

--- .config/luastatus/cryptocurr.lua
+++ .config/luastatus/cryptocurr.lua
@@ -2,7 +2,7 @@ widget = {
     plugin = 'timer',
     opts = {period = 600}, -- всё равно раз в день обновляется же
     cb = function()
-        return {full_text=luastatus.dollar{os.getenv('HOME') .. '/cryptocurr.bash'}, color='#BFB630'}
+        return {full_text=luastatus.dollar{os.getenv('HOME') .. '/.config/luastatus/cryptocurr.bash'}, color='#BFB630'}
     end,
 }
 
